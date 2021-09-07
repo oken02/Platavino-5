7aef6d07ed09b4c3e961216769a6c649cf898003
Marcobit93 <marcos93bolonese@gmail.com>
2021-09-07 11:03:48 -0300

    db.js con modelos, creacion de archivos modelos User.js Vino.js Carrito.js

--- api/models/Vino.js
+++ api/models/Vino.js
@@ -2,5 +2,26 @@ const S = require('sequelize');
 const db = require('../db');
 
 const Vino = db.define('vino', {
-    
+    PaisDeOrigen:{
+        type: S.STRING,
+    },
+    Bodega:{
+        type: S.STRING,
+    },
+    Precio:{
+        type: S.NUMBER,
+    },
+    Varietal:{
+        type: S.STRING,
+    },
+    Color:{
+        type: S.STRING,
+    },
+    ml:{
+        type: S.NUMBER,
+    },
+    Descripcion:{
+        type: S.STRING,
+    },
+        
 })
