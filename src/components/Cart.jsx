b3ce9fb7061ffb6da2161a02b69c2553dba62cb2
joselg94 <joselg_1994@hotmail.com>
2021-09-09 10:37:58 -0300

    agregando cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,144 +1,232 @@
-// import React from "react";
-
-// export default function Cart() {
-//   return (
-//     <div>
-
-//     </div>
-//   );
-// }
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
+import { Row, Column, Item } from "@mui-treasury/components/flex";
 import { makeStyles } from "@material-ui/core/styles";
+import {
+  Info,
+  InfoTitle,
+  InfoSubtitle,
+  InfoCaption,
+} from "@mui-treasury/components/info";
+import { useDynamicAvatarStyles } from "@mui-treasury/styles/avatar/dynamic";
+import { useD01InfoStyles } from "@mui-treasury/styles/info/d01";
+import grey from "@material-ui/core/colors/grey";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import { Icon } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { useHistory } from "react-router-dom";
+//necesito dispatchear(removeCarrito, addCarrito)
+//cuando este la db funcional traer la info con un useEffect para traer los productos que esten guardados en el carrito
+// aca para abajo es fakeinfo
+let probando = [
+  {
+    PaisDeOrigen: "Argentina ",
+    Bodega: "Alma Mora, Chivilcoy ",
+    Precio: 1500,
+    Varietal: "Malbec",
+    Color: "Tinto",
+    ml: 750,
+    Descripcion:
+      "Austero: es uno de los más valorados por los profesionales. Se usa para aquellos que expresan sin distorsiones el carácter varietal de las uvas y su origen o aquellos en los que la elaboración no desvirtúa su naturaleza. ",
+    Img: "https://static0.tiendeo.com.ar/upload_articulos/259644/0d89eaa5-db12-50d9-b06a-c7cda1c94a4b.jpg",
+  },
+  {
+    PaisDeOrigen: "Estados Unidos",
+    Bodega: "Antigal",
+    Precio: 25000,
+    Varietal: "Malbec",
+    Color: "Tinto",
+    ml: 750,
+    Descripcion:
+      "Balanceado: es el estado ideal de un vino en boca y significa que sus atributos (alcohol, taninos, acidez, fruta y dulzor) están en armonía. ",
+    Img: "https://www.antigal.com/wp-content/uploads/2020/09/antigal-key-visuals-28.jpg",
+  },
+  {
+    PaisDeOrigen: "Argentina ",
+    Bodega: "Alma Mora, Chivilcoy ",
+    Precio: 1500,
+    Varietal: "Malbec",
+    Color: "Tinto",
+    ml: 750,
+    Descripcion:
+      "Austero: es uno de los más valorados por los profesionales. Se usa para aquellos que expresan sin distorsiones el carácter varietal de las uvas y su origen o aquellos en los que la elaboración no desvirtúa su naturaleza. ",
+    Img: "https://static0.tiendeo.com.ar/upload_articulos/259644/0d89eaa5-db12-50d9-b06a-c7cda1c94a4b.jpg",
+  },
+  {
+    PaisDeOrigen: "Estados Unidos",
+    Bodega: "Antigal",
+    Precio: 25000,
+    Varietal: "Malbec",
+    Color: "Tinto",
+    ml: 750,
+    Descripcion:
+      "Balanceado: es el estado ideal de un vino en boca y significa que sus atributos (alcohol, taninos, acidez, fruta y dulzor) están en armonía. ",
+    Img: "https://www.antigal.com/wp-content/uploads/2020/09/antigal-key-visuals-28.jpg",
+  },
+];
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
+  item: {
+    backgroundColor: grey[900],
+    borderRadius: "5px",
+    marginBottom: "5px",
+    justifyContent: "space-between",
+    padding: "0 15px",
+  },
+  delete: {
+    color: "red",
+  },
+  row: {
+    height: "1000px",
+    width: "100%",
+  },
+  width: {
+    width: "100%",
+  },
+  column: {
+    width: "30%",
+    height: "50%",
+    backgroundColor: "gray",
+    marginLeft: "10px",
+    borderRadius: "3px",
+  },
   root: {
-    height: "100vh",
+    minWidth: 275,
   },
-  image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
-  paper: {
-    margin: theme.spacing(8, 4),
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  subtotal: {
     display: "flex",
-    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  card: {
+    display: "flex",
+    padding: "0",
+  },
+  icons: {
+    display: "flex",
     alignItems: "center",
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
 }));
+const Cart = React.memo(function DarkRapListItem() {
+  const avatarStyles = useDynamicAvatarStyles({ size: 70 });
+  const styles = useStyles();
+  const [total, setTotal] = useState(0);
+  const history = useHistory();
+  let contador = 1;
 
-export default function Cart() {
-  const classes = useStyles();
+  useEffect(() => {
+    let count = 0;
+    probando.map((wine) => {
+      count += wine.Precio;
+    });
+    setTotal(count);
+  }, []);
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+    <Row gap={5} className={styles.row}>
+      <div className={styles.width}>
+        {probando.map((wine, i) => {
+          return (
+            <Row key={i} className={styles.item}>
+              <Item className={styles.card}>
+                <Avatar
+                  variant={"rounded"}
+                  classes={avatarStyles}
+                  src={wine.Img}
+                  style={{ margin: "auto" }}
+                />
+                <Info useStyles={useD01InfoStyles}>
+                  <InfoCaption> • U$D {wine.Precio} #</InfoCaption>
+                  <InfoTitle>{wine.Bodega}</InfoTitle>
+                  <InfoSubtitle>• {wine.PaisDeOrigen} •</InfoSubtitle>
+                </Info>
+              </Item>
 
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
+              <div className={styles.icons}>
+                <Tooltip title="Delete">
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() =>
+                      console.log(
+                        "poner dispatch que elimina producto de carrito"
+                      )
+                    }
+                  >
+                    <DeleteIcon className={styles.delete} />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Remove">
+                  <IconButton
+                    aria-label="remove"
+                    onClick={() =>
+                      console.log(
+                        "poner dispatch que resta 1  producto de carrito"
+                      )
+                    }
+                  >
+                    <Icon color="secondary">remove_circle</Icon>
+                  </IconButton>
+                </Tooltip>
+                <Typography>{contador}</Typography>
+                <Tooltip title="Add">
+                  {/* agregar la logica de agregar tipo.. crear el producto en la base de datos y cuando lo queramos mostrar en el subtotal hacer un filter por el id  */}
+                  <IconButton
+                    aria-label="add"
+                    onClick={() => {
+                      console.log(
+                        "poner dispatch que suma 1  producto de carrito",
+                        contador
+                      );
+                    }}
+                  >
+                    <Icon color="primary">add_circle</Icon>
+                  </IconButton>
+                </Tooltip>
+              </div>
+            </Row>
+          );
+        })}
+      </div>
+
+      <Card className={(styles.root, styles.column)}>
+        <CardContent>
+          <Typography
+            className={styles.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Your Products:
           </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
+
+          <div className={styles.subtotal}>
+            <h5>Total:</h5>
+            <span>$ {total}</span>
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => history.push("/checkout")}>
+            Comprar
+          </Button>
+        </CardActions>
+      </Card>
+    </Row>
   );
-}
+});
+
+export default Cart;
--- src/components/Wine.jsx
+++ src/components/Wine.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import styles from "./Wine.module.css";
 import { IconButton } from "@material-ui/core";
-import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,8 +23,6 @@ function Wine() {
   //     });
   //   }, []);
 
-
-function Wine({ wineId }) {
   const defaultProps = {
     bgcolor: "background.paper",
     border: 1,
@@ -36,7 +32,6 @@ function Wine({ wineId }) {
   };
   const classes = useStyles();
   return (
-
     <>
       <div className={styles.detailsContainer}>
         <img
@@ -45,23 +40,20 @@ function Wine({ wineId }) {
           alt="foto vino"
         ></img>
 
+        <p> Precio: </p>
 
-
-
-
-          <p> Precio: </p>
-
-          <Button variant="contained" color="secondary">
-            Comprar
-          </Button>
-        </div>
+        <Button variant="contained" color="secondary">
+          Comprar
+        </Button>
+      </div>
 
       <div className={`${styles.col} ${styles.movieDetails}`}>
         <h1 className={styles.firstItem}> Nombre vino</h1>
         <p>
           <strong>Bodega:</strong>
         </p>
-        <p>wine
+        <p>
+          wine
           <strong>Color:</strong>
         </p>
         <p>
@@ -86,9 +78,8 @@ function Wine({ wineId }) {
         <IconButton color="primary" aria-label="add to shopping cart">
           {/* <AddShoppingCartIcon /> */}
         </IconButton>
-
       </div>
-    </div>
+    </>
   );
 }
 
