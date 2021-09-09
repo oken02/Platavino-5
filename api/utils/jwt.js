73f3c91677506d90b83fc5e9bc758120cf593b7c
kevin <kevin.c.quiroz>
2021-09-09 16:12:22 -0500

    saved

--- api/utils/jwt.js
+++ api/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (payload) => {
-  return jwt.sign({id:userid}, "SECRET", { expiresIn: "7d" });
+  return jwt.sign(payload, "SECRET", { expiresIn: "7d" });
 };
 
 const validateToken = (req, res, next) => {
